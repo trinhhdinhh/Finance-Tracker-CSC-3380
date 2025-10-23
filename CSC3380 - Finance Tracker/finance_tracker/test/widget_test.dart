1a2d0375c791c2f2365f253d3df63542cedfa494
trinhhdinhh <trdinh321@gmail.com>
2025-10-23 09:15:32 -0500

    Welcome Page
    Finished Welcome Page, don't be overwhelmed by everything in the files. This is default for flutter. The page is located in "lib".

---
+++ CSC3380 - Finance Tracker/finance_tracker/test/widget_test.dart
@@ -0,0 +1,23 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:finance_tracker/welcome_page.dart';
+
+void main() {
+  testWidgets('Finance Tracker app loads correctly', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const FinanceTrackerApp());
+
+    // Verify that the welcome page elements are displayed.
+    expect(find.text('Your Money,'), findsOneWidget);
+    expect(find.text('Simplified'), findsOneWidget);
+    expect(find.text('Get Started'), findsOneWidget);
+    expect(find.text('I Already Have an Account'), findsOneWidget);
+  });
+}
